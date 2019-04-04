9d1b9027167477c39420ec04b9f1d194ecf245b5
Ben Johnson <thisguyistotallyben@gmail.com>
2019-04-04 00:51:05 -0400

    adding/querying users in progress

--- hamshackhotline.py
+++ hamshackhotline.py
@@ -8,6 +8,11 @@ Purpose: Scrapes Hamshack Hotline's database pages to store users and bridges in
 
 import os
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
+
+
+USERS_URL = 'http://apps.wizworks.net:9091/results.php'
 
 
 class HamshackHotline:
@@ -29,7 +34,11 @@ class HamshackHotline:
         if 'bridge_table' not in kwargs:
             kwargs['bridge_table'] = 'hh_bridges'
 
+        if 'feature_table' not in kwargs:
+            kwargs['feature_table'] = 'hh_features'
+
         self.args = kwargs
+        print(self.args)
 
         self.start_database()
 
@@ -61,6 +70,13 @@ class HamshackHotline:
                                network text,
                                bridge number)''')
 
+        self.c.execute(f'''CREATE TABLE IF NOT EXISTS {self.args['feature_table']} (
+                               record_number text,
+                               name text,
+                               public text,
+                               network text,
+                               bridge number)''')
+
         self.conn.commit()
 
 
@@ -69,3 +85,76 @@ class HamshackHotline:
 
     Note: Be nice to Hamshack Hotline here. Try to keep fetches low.
     '''
+    def fetch_all(self):
+        self.fetch_users()
+        self.fetch_bridges()
+        self.fetch_features()
+
+    def fetch_users(self):
+        page = requests.get(USERS_URL)
+        
+        soup = BeautifulSoup(page.content, features='lxml')
+
+        e = []
+        first = True
+        for row in soup.find_all("tr"):
+            # remove header row
+            if first:
+                first = False
+                continue
+            
+            # actual data gathering
+            for col in row.find_all("td"):
+                if (len(col.contents) == 0):
+                    e.append('')
+                else:
+                    e.append(col.contents[0])
+
+            # PLEASE FOR THE LOVE OF EVERYTHING
+            # DON'T LET THIS DUPLICATE ENTRIES
+            # NOT COMMITING TO DATABASE BECAUSE OF THIS
+            self.c.execute(f"INSERT INTO {self.args['user_table']} VALUES ('{e[0]}', '{e[1]}', '{e[2]}', '{e[3]}', '{e[4]}', '{e[5]}', '{e[6]}', '{e[7]}', '{e[8]}')")
+            e.clear()
+
+    def fetch_bridges(self):
+        pass
+
+    def fetch_features(self):
+        pass
+
+
+    '''
+    Queries user table matching parameters from kwargs
+
+    possible kwargs:
+        callsign
+        number
+        name
+    '''
+    def query_users(self, **kwargs):
+        mult_args = False
+        query_args = []
+        query = f"SELECT * FROM {self.args['user_table']}"
+
+        # build string
+        if len(kwargs) != 0:
+            print('building string')
+            if 'callsign' in kwargs:
+                query = f"{query} WHERE callsign=?"
+                query_args.append(kwargs['callsign'])
+                mult_args = True
+            if 'number' in kwargs:
+                query = f"{query} {'AND ' if mult_args else ''}number=?"
+                query_args.append(kwargs['number'])
+                mult_args = True
+            if 'name' in kwargs:
+                query = f"{query} {'AND ' if mult_args else ''}name=?"
+                query_args.append(kwargs['name'])
+                mult_args = True
+
+        print(query)
+        print(tuple(query_args))
+
+        self.c.execute(query, query_args)
+
+        return self.c.fetchall()
